d7d744742894fbb796437b8b765e01ea1639192e
Allan Denot <adenot@gmail.com>
2025-07-10 09:21:08 +1000

    Handle when bastion sg is not created

--- eni.tf
+++ eni.tf
@@ -3,7 +3,7 @@ resource "aws_network_interface" "bastion_eni" {
   count           = var.create_eni ? 1 : 0
   subnet_id       = var.eni_subnet_id
   private_ips     = var.eni_private_ip != "" ? [var.eni_private_ip] : null
-  security_groups = concat([aws_security_group.bastion_host_security_group[0].id], var.bastion_additional_security_groups)
+  security_groups = concat(try([aws_security_group.bastion_host_security_group[0].id], []), var.bastion_additional_security_groups)
 
   tags = merge(
     var.tags,
